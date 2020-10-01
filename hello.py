5a456bd74ef04a0138c9538c90dffe00146ed672
JaneIllario <jane.illarionova@mail.utoronto.ca>
2020-10-01 03:23:15 -0400

    complete textbook form validate name exercise

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, session, redirect, url_for 
+from flask import Flask, render_template, session, redirect, url_for, flash 
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import Form
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = 'secret key in public repo!!'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-classNameForm(Form):
+class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
@@ -20,6 +20,9 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name']= form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
